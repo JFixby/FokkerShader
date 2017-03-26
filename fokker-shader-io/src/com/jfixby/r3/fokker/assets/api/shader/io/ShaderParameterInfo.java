e106f212fdf7ef9a58e545c732e234fe3a5f4018
J Fixby <github@jfixby.com>
2017-03-26 11:06:45 +0200

    ...

--- fokker-shader-io/src/com/jfixby/r3/fokker/assets/api/shader/io/ShaderParameterInfo.java
+++ fokker-shader-io/src/com/jfixby/r3/fokker/assets/api/shader/io/ShaderParameterInfo.java
@@ -5,6 +5,10 @@ import java.io.Serializable;
 
 public class ShaderParameterInfo implements Serializable {
 	private static final long serialVersionUID = 8458598392106057254L;
+
+	public static final String TYPE_FLOAT = "float";
+	public static final String TYPE_INT = "int";
+
 	public String name;
 	public String type;
 
