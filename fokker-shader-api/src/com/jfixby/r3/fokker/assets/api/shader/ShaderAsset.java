b5825c92a4f207df547464dedd770dac2718380b
J Fixby <github@jfixby.com>
2017-04-16 11:02:11 +0200

    type

--- fokker-shader-api/src/com/jfixby/r3/fokker/assets/api/shader/ShaderAsset.java
+++ fokker-shader-api/src/com/jfixby/r3/fokker/assets/api/shader/ShaderAsset.java
@@ -1,16 +1,12 @@
 
 package com.jfixby.r3.fokker.assets.api.shader;
 
+import com.jfixby.r3.fokker.api.ShaderParameter;
+import com.jfixby.rana.api.Asset;
 import com.jfixby.scarabei.api.collections.Mapping;
 
-public interface ShaderAsset {
-
-	public VertexProgram getVertexProgram ();
-
-	public FragmentProgram getFragmentProgram ();
+public interface ShaderAsset extends Asset {
 
 	public Mapping<String, ShaderParameter> listParameters ();
-
-	public boolean isOverlay ();
 
 }
