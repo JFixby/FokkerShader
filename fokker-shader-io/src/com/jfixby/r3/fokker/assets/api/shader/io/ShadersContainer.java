b90565d053826ac7d0993f4146b3b6d25c6fd7a1
J Fixby <github@jfixby.com>
2017-04-30 10:56:04 +0200

    +id

--- fokker-shader-io/src/com/jfixby/r3/fokker/assets/api/shader/io/ShadersContainer.java
+++ fokker-shader-io/src/com/jfixby/r3/fokker/assets/api/shader/io/ShadersContainer.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.r3.fokker.assets.api.shader.io;
 
 import java.io.Serializable;
@@ -5,6 +6,10 @@ import java.util.ArrayList;
 
 public class ShadersContainer implements Serializable {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 8943657452459305582L;
 	//
 	public ArrayList<ShaderInfo> shaders = new ArrayList<ShaderInfo>();
 
