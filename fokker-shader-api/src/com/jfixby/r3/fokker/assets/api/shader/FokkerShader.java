dfcd826be38c06a9589cc3d51588a4d39402c50a
J Fixby <github@jfixby.com>
2017-06-10 10:46:06 +0200

    ...

--- fokker-shader-api/src/com/jfixby/r3/fokker/assets/api/shader/FokkerShader.java
+++ fokker-shader-api/src/com/jfixby/r3/fokker/assets/api/shader/FokkerShader.java
@@ -1,15 +1,16 @@
 
 package com.jfixby.r3.fokker.assets.api.shader;
 
+import com.jfixby.r3.fokker.api.Shader;
 import com.jfixby.r3.fokker.api.ShaderSettings;
 
-public interface FokkerShader {
+public interface FokkerShader extends Shader {
 
 	com.badlogic.gdx.graphics.glutils.ShaderProgram getGdxShaderProgram ();
 
 	boolean isOverlay ();
 
-	void applyParameters (ShaderSettings params);
+	void applySettings (ShaderSettings params);
 
 	void setOpacity (double opacity);
 
